22992f3d4616eb3819e4e2a231ecc91802adea06
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-25 00:21:43 +0530

    final commit and check after run code successfully run in SavingsAccount.java

--- src/main/java/com/niit/jdp/SavingsAccountImpl.java
+++ src/main/java/com/niit/jdp/SavingsAccountImpl.java
@@ -8,6 +8,9 @@ package com.niit.jdp;
 public class SavingsAccountImpl {
     public static void main ( String[] args ) {
         SavingsAccount savingsAccount = new SavingsAccount ( );
+        System.out.println ( "accountHolderName = " + savingsAccount.AccountHolderName );
+        System.out.println ( "accountNumber = " + savingsAccount.AccountNumber );
+        System.out.println ( "accountStatus = " + savingsAccount.AccountStatus );
     
         savingsAccount.retrieveTheAccountBalance ( );
         savingsAccount.debitCash ( 50000.0 );
