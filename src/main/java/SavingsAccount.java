b8cd02c5cf5e495ad43c94febf8f93f17021ac62
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-24 17:40:46 +0530

    Recreate a SavingsAccount main file class

--- src/main/java/SavingsAccount.java
+++
@@ -1,8 +0,0 @@
-/*
- * Author Name: Mohit Saini
- * Date: 24-08-2022
- * Created With: IntelliJ IDEA Community Edition
- */
-
-public class SavingsAccount {
-}
--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -3,6 +3,7 @@
  * Date: 24-08-2022
  * Created With: IntelliJ IDEA Community Edition
  */
+package com.niit.jdp;
 
-public class SavingsAccountImpl {
+public class SavingsAccount {
 }
